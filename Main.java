063719f0ad4637379de782ebc1498e799308ef15
brown-lyssie <abrown@nevada.unr.edu>
2023-10-24 17:43:31 -0700

    finished

--- Main.java
+++ Main.java
@@ -3,8 +3,76 @@ public class Main {
 	public static void main(String[] args){
 
 		Logger loggingBase = Logger.getInstance();
+        
+        loggingBase.setLevel("ALL");
+        System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
+
         loggingBase.setLevel("DEBUG");
         System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
+
+        loggingBase.setLevel("INFO");
+        System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
+
+        loggingBase.setLevel("WARN");
+        System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
+
+        loggingBase.setLevel("ERROR");
+        System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
+
+        loggingBase.setLevel("FATAL");
+        System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
+
+        loggingBase.setLevel("OFF");
+        System.out.println(loggingBase.getLevel());
+
+        loggingBase.trace("Trace");
+        loggingBase.debug("Debug");
+        loggingBase.info("Info");
+        loggingBase.warn("Warn");
+        loggingBase.error("Error");
+        loggingBase.fatal("Fatal");
 	
 	}
 }
